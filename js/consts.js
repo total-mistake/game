5264e00f864f88baf7eb8cb7a4140779fee544a1
Suhanova Elizaveta <suhanova.ea@edu.spbstu.ru>
2024-02-24 07:45:47 +0300

    Добавлены главная страница и выбор уровня

---
+++ js/consts.js
@@ -0,0 +1,36 @@
+const plantsPath = "./elements/light/";
+const plants = [
+    {
+        pot: false
+    },
+    {
+        pot: true
+    },
+    {
+        pot: true
+    },
+    {
+        pot: true
+    },
+    {
+        pot: false
+    },
+    {
+        pot: false
+    },
+    {
+        pot: true
+    },
+    {
+        pot: true
+    },
+    {
+        pot: true
+    },
+    {
+        pot: true
+    },
+    {
+        pot: false
+    }
+]
